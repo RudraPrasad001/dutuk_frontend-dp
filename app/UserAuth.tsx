af99cbae8e607948ae2aaf491a9e6fc4bcd2a932
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-05-27 19:33:58 +0530

    modularized css for auth button

--- app/UserAuth.tsx
+++ app/UserAuth.tsx
@@ -1,35 +1,16 @@
+import authButtonStyle from "@/css/authButtonStyle";
 import React from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 const UserAuth = () => {
-  const userAuthStyle = StyleSheet.create({
-    button: {
-      width: 250,
-      height: 75,
-      backgroundColor: "black",
-      justifyContent: "center",
-      borderRadius: 50,
-
-      elevation: 10,
-
-      margin: 5,
-    },
-    buttonText: {
-      color: "white",
-      textAlign: "center",
-      fontSize: 20,
-      fontWeight: "bold",
-    },
-  });
-
   return (
     <View>
-      <Pressable style={userAuthStyle.button}>
-        <Text style={userAuthStyle.buttonText}>Login</Text>
+      <Pressable style={authButtonStyle.button}>
+        <Text style={authButtonStyle.buttonText}>Login</Text>
       </Pressable>
 
-      <Pressable style={userAuthStyle.button}>
-        <Text style={userAuthStyle.buttonText}>Register</Text>
+      <Pressable style={authButtonStyle.button}>
+        <Text style={authButtonStyle.buttonText}>Register</Text>
       </Pressable>
     </View>
   );
---
+++ css/authButtonStyle.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const authButtonStyle = StyleSheet.create({
+  button: {
+    width: 250,
+    height: 75,
+    backgroundColor: "black",
+    justifyContent: "center",
+    borderRadius: 50,
+
+    elevation: 10,
+
+    margin: 5,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+
+export default authButtonStyle;
