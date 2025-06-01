721d7db1ab2e1c5a055f28d9ec0926eb856423cc
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-06-01 23:35:54 +0530

    Implimented better error handling for login

--- hooks/useLoginUser.ts
+++ hooks/useLoginUser.ts
@@ -2,17 +2,33 @@ import { supabase } from "@/utils/supabase";
 import { router } from "expo-router";
 import { Alert } from "react-native";
 
-const loginUser = async (userEmail: any, userPassword: any) => {
+const loginUser = async (userEmail: string, userPassword: string) => {
   const { error } = await supabase.auth.signInWithPassword({
     email: userEmail,
     password: userPassword,
   });
+
   if (error) {
-    Alert.alert("Error when trying to log in");
-  } else {
-    Alert.alert("Login success!");
-    router.replace("/(tabs)/home");
+    const message = error.message.toLowerCase();
+
+    if (message.includes("invalid login credentials")) {
+      Alert.alert("Login Failed", "Wrong email or password.");
+    } else if (message.includes("user not found")) {
+      Alert.alert("Login Failed", "User does not exist.");
+    } else if (message.includes("email not confirmed")) {
+      Alert.alert(
+        "Login Failed",
+        "Please verify your email before logging in."
+      );
+    } else {
+      Alert.alert("Login Failed", error.message);
+    }
+
+    return;
   }
+
+  Alert.alert("Login success!");
+  router.replace("/(tabs)/home");
 };
 
 export default loginUser;
