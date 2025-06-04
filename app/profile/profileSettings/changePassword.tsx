d7a83d0761107e08de1c0271c8e8f4a616800a9d
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-06-04 20:30:27 +0530

    Profile Settings Routes Done

--- app/profile/profileSettings/changePassword.tsx
+++ app/profile/profileSettings/changePassword.tsx
@@ -1,11 +1,11 @@
 import { Text, View } from "react-native";
 
 
-const ProfileSettings = ()=>{
+const ChangePassword = ()=>{
     return(
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text style={{ fontWeight: "bold", fontSize: 50 }}>ProfileSettings</Text>
+            <Text style={{ fontWeight: "bold", fontSize: 50 }}>Change Password</Text>
         </View>
     )
 }
-export default ProfileSettings;
+export default ChangePassword;
---
+++ app/profile/profileSettings/changeUsername.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+
+const ChangeUsername = ()=>{
+    return(
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ fontWeight: "bold", fontSize: 50 }}>ChangeUsername</Text>
+        </View>
+    )
+}
+export default ChangeUsername;
